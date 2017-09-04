36190c2ff30ab2e26d39cbcab70570137d00b863
Lars Vidar <larsvidar@larsvidar.no>
2017-09-05 00:03:25 +0200

    - Getting random url from API insted of Special:Random. - Made the 'Open article' button work. - Made function for checking window-size before choosing mobile or desktop version of the page. - Cleaned up javascript with comments.

--- js/wikisearch.js
+++ js/wikisearch.js
@@ -1,1 +1,33 @@
 console.log("Wikisearch.js loaded!");
+
+var apiRandomWikiUrl = "https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&format=json&callback=?"; // URL for API-request
+var articleId; //Variable to hold Wikipedia article ID.
+var randomURL = "https://en.m.wikipedia.org/?curid=" //Base URL to use width article ID.
+
+
+/***** Function for passing randomURL to #iframe and #open-button *****/
+function setIframe(url) {
+	$("#iframe").attr("src", url);
+	$("#open-button").attr("href", url);
+}
+
+/***** Function for checking screensize and using mobile version on small screens *****/
+function checkScreen() {
+	if (window.innerWidth > 760) {
+		return "https://en.wikipedia.org/?curid="
+	} else {
+		return "https://en.m.wikipedia.org/?curid="
+	}
+}
+
+/*************** Requesting random page via API ***************/
+$.getJSON(apiRandomWikiUrl, function(data) {
+	articleId = (data.query.random[0].id);
+	randomURL = checkScreen(randomURL);
+	randomURL += articleId;
+	setIframe(randomURL);
+});
+
+
+
+
